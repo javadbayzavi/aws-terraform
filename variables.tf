3cfc94f02cda289d494e245dd057b4e4e1361304
Javad Bayzavi <javadbayzavi@gmail.com>
2025-10-13 18:05:43 +0200

    Define very basic variables for the project

--- variables.tf
+++ variables.tf
@@ -3,3 +3,55 @@ variable "aws_region" {
   description = "The AWS region to deploy resources in."
   default     = "us-east-1" # Change this to your desired region
 }
+
+variable "vpc_name" {
+  type = string
+  description = "The name of the VPC."
+  default = "my-vpc"
+}
+
+variable "subnet_name" {
+  type = string
+  description = "The name of the subnet."
+  default = "my-subnet"
+}
+
+variable "vpc_ids" {
+  type = list(string)
+  description = "List of VPC IDs to associate with the security group."
+  default = []
+}
+
+variable "subnet_ids" {
+  type = list(string)
+  description = "List of Subnet IDs to associate with the security group."
+  default = []
+}
+
+variable "project_name" {
+  type        = string
+  description = "The name of the project."
+  default     = "my-project"
+}
+
+variable "tags" {
+  type = map(string)
+  description = "A map of tags to assign to the resources."
+  default = {
+  }
+}
+variable "environemt" {
+  type        = string
+  description = "The environment for the resources (e.g., dev, staging, prod)."
+  default     = "dev"
+}
+
+locals {
+  merged_tags = merge(
+    var.tags,
+    {
+      Project = var.project_name
+      Environment     = var.environemt
+    }
+  )
+}
