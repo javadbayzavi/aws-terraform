0200e0a242cc2dd9085659fadce2af6e3e4556eb
Javad Bayzavi <javadbayzavi@gmail.com>
2025-10-14 17:22:54 +0200

    Now we can simply deploy vpc resources to aws

--- variables.tf
+++ variables.tf
@@ -11,42 +11,8 @@ variable "subnet_name" {
   default = "my-subnet"
 }
 
-variable "vpc_ids" {
-  type = list(string)
-  description = "List of VPC IDs to associate with the security group."
-  default = []
-}
-
-variable "subnet_ids" {
-  type = list(string)
-  description = "List of Subnet IDs to associate with the security group."
-  default = []
-}
-
-variable "project_name" {
-  type        = string
-  description = "The name of the project."
-  default     = "my-project"
-}
-
-variable "tags" {
-  type = map(string)
-  description = "A map of tags to assign to the resources."
-  default = {
-  }
-}
 variable "environemt" {
   type        = string
   description = "The environment for the resources (e.g., dev, staging, prod)."
   default     = "dev"
-}
-
-locals {
-  merged_tags = merge(
-    var.tags,
-    {
-      Project = var.project_name
-      Environment     = var.environemt
-    }
-  )
 }
